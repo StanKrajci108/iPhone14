7666e283ded89398f882389292df8240dd28002d
StanKrajci108 <krajci.s@tutanota.com>
2024-02-27 21:25:57 +0100

    commit

--- script.js
+++ script.js
@@ -1,37 +1,4 @@
-// let purple = document.querySelectorAll(".fialova");
-// let gold = document.querySelectorAll(".zlata");
-// let silver = document.querySelectorAll(".stribrna");
-// let black = document.querySelectorAll(".cerna");
-
-// purple.forEach(function (element) {
-//   element.addEventListener("click", function () {
-//     let imgElement = document.querySelector(".img img");
-//     imgElement.src = "images/iphone-purple.jpg";
-//   });
-// });
-
-// gold.forEach(function (element) {
-//   element.addEventListener("click", function () {
-//     let imgElement = document.querySelector(".img img");
-//     imgElement.src = "images/iphone-gold.jpg";
-//   });
-// });
-
-// silver.forEach(function (element) {
-//   element.addEventListener("click", function () {
-//     let imgElement = document.querySelector(".img img");
-//     imgElement.src = "images/iphone-silver.jpg";
-//   });
-// });
-
-// black.forEach(function (element) {
-//   element.addEventListener("click", function () {
-//     let imgElement = document.querySelector(".img img");
-//     imgElement.src = "images/iphone-black.jpg";
-//   });
-// });
-
-// Path: script.js
+// Function to add click listeners
 function addColorClickListener(colorClass, imagePath) {
   let elements = document.querySelectorAll(colorClass);
   elements.forEach(function (element) {
